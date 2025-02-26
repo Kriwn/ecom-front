b30764536746bc9a85c9a3c13329efbbdde6b347
Kriwn <nonisgod@gmail.com>
2025-02-27 03:40:37 +0700

    add env example

--- app/utils/session.ts
+++ app/utils/session.ts
@@ -1,9 +1,11 @@
 import { createCookieSessionStorage } from "@remix-run/node";
 
+const secrets = process.env.SESSION_SECRET as string;
+
 const { getSession, commitSession, destroySession } = createCookieSessionStorage({
   cookie: {
     name: "session",
-    secrets: ["your-secret-key"],
+    secrets: [secrets],
     sameSite: "lax",
     httpOnly: true,
     secure: true,
